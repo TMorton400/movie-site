749f4dbb5169ade0f318901eac3a90cd5cdf8572
TMorton400 <mortontaye@gmail.com>
2024-09-01 14:39:46 +0100

    new hamburger menu

--- movie.js
+++ movie.js
@@ -41,4 +41,17 @@ const displayMovies = (movies) => {
   });
 }
 
+const popMenu= () => {
+  let x = document.getElementById("list");
+  let y =document.getElementById("navBar");
+  if(x.style.display === "none"){
+    x.style.display = "flex";
+    y.style.height = "300px";
+  }
+  else{
+    x.style.display = "none";
+    y.style.height = "165px";
+  }
+}
+
 popularMovie();
