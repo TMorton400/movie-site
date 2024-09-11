40a2fb64939aa09091fb63707551487eaf107751
TMorton400 <mortontaye@gmail.com>
2024-09-11 19:30:04 +0100

    Completed links for drop down menu list items

--- assets/js/actor.js
+++ assets/js/actor.js
@@ -105,24 +105,4 @@ const findActors = () => {
 
 
 
-
-
-
-
-
-const popMenu= () => {
-  let x = document.getElementById("list");
-  let y =document.getElementById("navBar");
-  if(x.style.display === "none"){
-    x.style.display = "flex";
-    y.style.height = "300px";
-  }
-  else{
-    x.style.display = "none";
-    y.style.height = "165px";
-  }
-}
-
-
-
 popularActor();
--- assets/js/movie.js
+++ assets/js/movie.js
@@ -115,18 +115,5 @@ const findMovie = () => {
   }
 }
 
-// Used in responsive web design to ensure the hamburger menu works
-const popMenu= () => {
-  let x = document.getElementById("list");
-  let y =document.getElementById("navBar");
-  if(x.style.display === "none"){
-    x.style.display = "flex";
-    y.style.height = "300px";
-  }
-  else{
-    x.style.display = "none";
-    y.style.height = "165px";
-  }
-}
 
 popularMovie();
